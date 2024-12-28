53e4280e3c5e77f2456d8cc049b0270947acc38b
Sponsoparnordvpn <146754550+Sponsoparnordvpn@users.noreply.github.com>
2024-12-28 10:55:54 +0100

    better perfs

--- modules/utils/bit32.lua
+++ modules/utils/bit32.lua
@@ -18,15 +18,7 @@ function bit32:band(a, b)
 end
 
 function bit32:bor(a, b)
-    local result = 0
-    local shift = 1
-    for i = 0, 31 do
-        if (a % 2 == 1 or b % 2 == 1) then result = result + shift end
-        a = floor(a / 2)
-        b = floor(b / 2)
-        shift = shift * 2
-    end
-    return result
+    return bit32:bxor(a, b)
 end
 
 function bit32:bxor(a, b)
@@ -40,5 +32,6 @@ function bit32:bxor(a, b)
     end
     return result
 end
+
 
 return bit32
--- readme.md
+++ readme.md
@@ -82,3 +82,21 @@ Contributions are welcome! Please follow these steps:
 ## 📜 License
 
 This project is licensed under the MIT License. See the `LICENSE` file for more details.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
